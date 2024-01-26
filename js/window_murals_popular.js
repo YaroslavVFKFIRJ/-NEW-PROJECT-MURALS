16c07eb1d3288baa078dd37af56c8ae6f2f5aafa
YaroslavVFKFIRJ <150508580+YaroslavVFKFIRJ@users.noreply.github.com>
2024-01-26 13:28:57 +0300

    Update window_murals_popular.js

--- js/window_murals_popular.js
+++ js/window_murals_popular.js
@@ -35,13 +35,3 @@ let mwind=document.querySelector('.mwind');
                }
             }
         });
-        // crossdate.onclick=()=>{
-        //     mwind.classList.remove('vis');
-        //     blur.classList.remove('vis');
-        //     image.classList.remove(classb);
-        //     for(let q=0; q<=arrhref.length; q++){
-        //         if(el==arrhref[q]) arrname[q].classList.toggle('vis')
-        //         else continue;
-        // }
-    
-        
